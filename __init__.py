074dd3a32002d85c8eea7ec37b5b2b63698f8e57
ahawker <andrew.r.hawker@gmail.com>
2012-11-27 21:05:24 -0800

    Added client/server stubs.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 import os
 import sys
 
-#path purity be damned!
-sys.path.append(os.path.dirname(os.path.abspath(__file__))
+__author__ = 'ahawker'
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+GEVENT_ROOT = os.path.join(ROOT, r'gevent')
+GREENLET_ROOT = os.path.join(ROOT, r'greenlet')
+LIBEVENT_ROOT = os.path.join(ROOT, r'libevent')
---
+++ kettle/dht/kademlia/client.py
@@ -0,0 +1,4 @@
+__author__ = 'ahawker'
+
+class Client(object):
+    pass
