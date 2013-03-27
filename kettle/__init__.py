881558b03f2a8d4f798f8e2fda434de3c4231708
ahawker <andrew.r.hawker@gmail.com>
2013-03-26 18:48:02 -0700

    Added package level packages for tox/travis integration, setup/manifest for pip integration.

---
+++ kettle/__init__.py
@@ -0,0 +1,3 @@
+__name__    = 'kettle'
+__version__ = '0.0.1'
+__author__  = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
---
+++ setup.py
@@ -0,0 +1,31 @@
+__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+
+import kettle
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name=crython.__name__,
+    version=crython.__version__,
+    description='Cron scheduling for python functions.',
+    long_description=open('README.md').read(),
+    author='Andrew Hawker',
+    author_email='andrew.r.hawker@gmail.com',
+    url='https://github.com/ahawker/crython',
+    license=open('LICENSE.md').read(),
+    package_dir={'crython': 'crython'},
+    packages=['crython'],
+    test_suite='tests',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        )
+)
