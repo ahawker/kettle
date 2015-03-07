f39d3bced7aaada5080732a07b3e5509f2b3166c
Andrew Hawker <andrew.r.hawker@gmail.com>
2015-03-06 23:39:04 -0800

    Untrack tests files until refactor complete.

--- tests/fixtures.py
+++
@@ -1,8 +0,0 @@
-__author__  = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
-
-from kettle.kettle import Node
-
-default_address = '127.0.0.1'
-default_port = 9995
-default_id = 0
-default_node = Node(default_address, default_port, default_id)
